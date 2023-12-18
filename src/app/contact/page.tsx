e24aed7c061ad9bfe3d0189f309afb5fe45ca630
Bogdan Sinik <bogdansinik2001@gmail.com>
2023-12-18 23:20:15 +0100

    Add new pages for About Us, Contact, and Support Us

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,66 @@
+// components/Contact.tsx
+import React from "react";
+import Head from "next/head";
+
+const Contact: React.FC = () => {
+    return (
+        <main className="bg-[#ECE3CE]">
+            <div className="contact-container bg-gray-100 p-8 rounded-md shadow-md max-w-2xl mx-auto flex items-center">
+                <div className="text-left pr-8">
+                    <h1 className="text-4xl font-bold mb-6 text-gray-800">
+                        Sustainability Park Istra
+                    </h1>
+
+                    <div className="mb-6">
+                        <h2 className="text-2xl font-bold mb-2 text-gray-700">Contact</h2>
+                        <p className="text-gray-700">
+                            For general inquiries, please email us at{" "}
+                            <a
+                                href="mailto:info@parkistra.com"
+                                className="text-blue-500 hover:underline"
+                            >
+                                info@parkistra.com
+                            </a>
+                            .
+                        </p>
+                    </div>
+
+                    <div className="mb-6">
+                        <h2 className="text-2xl font-bold mb-2 text-gray-700">For Volunteers</h2>
+                        <p className="text-gray-700">
+                            If you are interested in volunteering, please contact us at{" "}
+                            <a
+                                href="mailto:volunteers@parkistra.com"
+                                className="text-blue-500 hover:underline"
+                            >
+                                volunteers@parkistra.com
+                            </a>
+                            .
+                        </p>
+                    </div>
+
+                    <div>
+                        <h2 className="text-2xl font-bold mb-2 text-gray-700">Telephone</h2>
+                        <p className="text-gray-700">
+                            Reach us by phone at{" "}
+                            <a href="tel:0038651212921" className="text-blue-500 hover:underline">
+                                00 386 51 212 921
+                            </a>
+                            .
+                        </p>
+                    </div>
+                </div>
+
+                <div className="flex-shrink-0">
+                    <img
+                        src="/contact.jpg"
+                        alt="Contact Image"
+                        className="rounded-full w-52 h-52 object-cover"
+                    />
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default Contact;
---
+++ src/app/support/page.tsx
@@ -0,0 +1,68 @@
+// components/SupportUs.tsx
+import React from 'react';
+import Head from 'next/head';
+
+const SupportUs: React.FC = () => {
+  return (
+    <>
+    <main className='bg-[#ECE3CE]'>
+      <Head>
+        <title>Support Us | Park Istra</title>
+        <meta name="description" content="Support Park Istra by making a donation. Find information on wire transfers and use the provided form to submit your donation. Thank you for your support!" />
+      </Head>
+      <div className="support-us-container bg-gray-100 p-8 rounded-md shadow-md max-w-2xl mx-auto flex items-center">
+        <div className="text-left pr-8">
+          <h1 className="text-4xl font-bold mb-6 text-gray-800">Support Park Istra</h1>
+
+          <p className="text-gray-700 mb-6">
+            You can contribute to our cause by making a donation through wire transfer. Here are the details:
+          </p>
+
+          <div className="mb-6">
+            <h2 className="text-2xl font-bold mb-2 text-gray-700">Wire Transfer Details</h2>
+            <p className="text-gray-700">
+              <strong>Recipient:</strong> Park Istra<br />
+              <strong>Address:</strong> Kastelec 21, 6275 Črni Kal<br />
+              <strong>IBAN:</strong> SI56 6100 0001 3661 536 (Delavska hranilnica d.d., Ljubljana)<br />
+              <strong>SWIFT/BIC:</strong> HDELSI22<br />
+              <strong>SEPA Purpose Code:</strong> CHAR<br />
+              <strong>Purpose:</strong> Donation
+            </p>
+          </div>
+
+          <p className="text-gray-700 mb-6">
+            THANK YOU FOR YOUR SUPPORT!
+          </p>
+
+          {/* Donation Form (Placeholder) */}
+          <form className="mb-6">
+            {/* Your donation form fields go here */}
+            <label className="block text-gray-700 font-bold mb-2" htmlFor="donationAmount">
+              Donation Amount:
+            </label>
+            <input
+              type="text"
+              id="donationAmount"
+              name="donationAmount"
+              className="w-full border border-gray-300 p-2 rounded-md"
+              placeholder="Enter amount in EUR"
+            />
+
+            {/* Add more form fields as needed */}
+
+            <button
+              type="submit"
+              className="mt-4 bg-blue-500 text-white p-2 rounded-md hover:bg-blue-700"
+            >
+              Submit Donation
+            </button>
+          </form>
+        </div>
+      </div>
+      
+      </main>
+    </>
+  );
+};
+
+export default SupportUs;
