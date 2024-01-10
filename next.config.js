889aa1e8954d590d9afe6d2094b211bb889af0ed
Nikola <kovacevicnikola@protonmail.com>
2024-01-10 16:43:38 +0100

    Update domains in next.config.js

--- next.config.js
+++ next.config.js
@@ -22,10 +22,11 @@ const nextConfig = {
             },
         ],
         domains: [
-            "scontent.cdninstagram.com",
-            "graph.instagram.com",
-            "scontent-iad3-2.cdninstagram.com",
-            "scontent-iad3-1.cdninstagram.com",
+            // "scontent.cdninstagram.com",
+            // "graph.instagram.com",
+            // "scontent-iad3-2.cdninstagram.com",
+            // "scontent-iad3-1.cdninstagram.com",
+            "*.cdninstagram.com",
         ],
     },
 };
